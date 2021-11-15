64001d99f943d0826c8c7643fec899ebdf5b32e8
Bei Zhou <acw554@frontend11.apocrita>
2021-11-15 17:11:17 +0000

    update train.sh

--- Attention/train.sh
+++ Attention/train.sh
@@ -16,10 +16,10 @@ module load cudnn/7.6-cuda-10.1
 source ~/project/venv/bin/activate
 
 # run the python script
-for n_layers in 1 2 3; do
-python train_self_attention.py --n_elems=15 \
-                               --n_train_elems=10 \
-                               --n_train_samples=128000 \
+for n_layers in 1 2 3 4 5 6 7 8 9; do
+python train_self_attention.py --n_elems=30 \
+                               --n_train_elems=25 \
+                               --n_train_samples=256000 \
                                --n_eval_samples=10000 \
                                --n_exclusive_data=0 \
                                --n_epochs=100 \
