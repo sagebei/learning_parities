899596d526f285edf52043738f0e29d333473020
sagebei <bei.zhou@qmul.ac.uk>
2021-11-15 15:29:57 +0000

    add attention model

--- Attention/train.sh
+++ Attention/train.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+#$ -cwd
+#$ -j y
+#$ -pe smp 8        # Request cores (8 per GPU)
+#$ -l h_vmem=7.5G   # Request RAM (7.5GB per core)
+#$ -l h_rt=48:0:0    # Max 1hr runtime (can request up to 240hr)
+#$ -l gpu=1         # Request GPU
+#$ -N attention      # Name for the job (optional)
+
+# Load the necessary modules
+module load python/3.8.5
+module load cudnn/7.6-cuda-10.1
+
+# Load the virtualenv containing the pytorch package
+source ~/project/venv/bin/activate
+
 # run the python script
-#
 for n_layers in 1 2 3; do
 python train_self_attention.py --n_elems=15 \
                                --n_train_elems=10 \
