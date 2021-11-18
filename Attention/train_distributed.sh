45f7b951bfe82b5393159c34993ee84703128e10
sagebei <bei.zhou@qmul.ac.uk>
2021-11-18 10:24:51 +0000

    update the bash files

--- Attention/train_distributed.sh
+++ Attention/train_distributed.sh
@@ -17,20 +17,20 @@ source ~/venv/bin/activate
 
 # run the python script
 for n_layers in 1 2 3 4 5 6 7 8 9 10 11 12 13 15 16; do
-python train_self_attention.py --n_elems=60 \
-                               --n_train_elems=55 \
-                               --n_train_samples=512000 \
-                               --n_eval_samples=10000 \
-                               --n_exclusive_data=0 \
-                               --n_epochs=100 \
-                               --batch_size=128 \
-                               --n_layers=$n_layers \
-                               --train_unique='' \
-                               --mode='soft' \
-                               --embed_dim=30 \
-                               --n_heads=3 \
-                               --linear_dim=30 \
-                               --dropout=0.2 \
-                               --log_folder='results_dist60' \
-                               --num_workers=2
+python train_self_attention_distributed.py --n_elems=60 \
+                                           --n_train_elems=55 \
+                                           --n_train_samples=512000 \
+                                           --n_eval_samples=10000 \
+                                           --n_exclusive_data=0 \
+                                           --n_epochs=100 \
+                                           --batch_size=128 \
+                                           --n_layers=$n_layers \
+                                           --train_unique='' \
+                                           --mode='soft' \
+                                           --embed_dim=30 \
+                                           --n_heads=3 \
+                                           --linear_dim=30 \
+                                           --dropout=0.2 \
+                                           --log_folder='results_dist60' \
+                                           --num_workers=2
 done;
