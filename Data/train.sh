f3d1b7b3054b95f8d1d1be86a641408f21a8313b
sagebei <bei.zhou@qmul.ac.uk>
2021-11-29 13:25:20 +0000

    Update

--- Data/train.sh
+++ Data/train.sh
@@ -7,7 +7,7 @@
 #$ -l h_rt=48:0:0    # Max 1hr runtime (can request up to 240hr)
 #$ -l gpu=1         # Request GPU
 #$ -l cluster=andrena  # Ensure that the job runs on Andrena nodes
-#$ -N lstm      # Name for the job (optional)
+#$ -N data      # Name for the job (optional)
 
 # Load the necessary modules
 module load python/3.8.5
@@ -16,15 +16,15 @@ module load cudnn/8.1.1-cuda11
 # Load the virtualenv containing the pytorch package
 source ~/venv/bin/activate
 
-for n_layer in 1 2 3 4 5 6 7 8 9; do
-python train_lstm.py --n_elems=200 \
-                     --n_train_elems=195 \
-                     --n_train_samples=2048000 \
+for data_augmentation in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8; do
+python train_lstm.py --n_elems=30 \
+                     --n_train_elems=30 \
+                     --n_train_samples=100000 \
                      --n_eval_samples=10000 \
                      --n_epochs=200 \
-                     --n_layers=$n_layer \
+                     --n_layers=1 \
                      --train_unique='' \
                      --n_exclusive_data=0 \
-                     --data_augmentation=0 \
-                     --log_folder='results200'
+                     --data_augmentation=$data_augmentation \
+                     --log_folder='results'
 done;
