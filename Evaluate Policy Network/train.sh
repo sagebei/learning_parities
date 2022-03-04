bfff48a05eec2bb97baf6528125853638d8ff3c8
sagebei <bei.zhou@qmul.ac.uk>
2022-03-04 11:23:27 +0000

    Update

---
+++ Evaluate Policy Network/train.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#$ -cwd
+#$ -j y
+#$ -pe smp 8        # Request cores (8 per GPU)
+#$ -l h_vmem=7.5G   # Request RAM (7.5GB per core)
+#$ -l h_rt=24:0:0    # Max 1hr runtime (can request up to 240hr)
+#$ -l gpu=1         # Request GPU
+#$ -l cluster=andrena  # Ensure that the job runs on Andrena nodes
+#$ -N lstm      # Name for the job (optional)
+
+# Load the necessary modules
+module load python/3.8.5
+module load cudnn/8.1.1-cuda11
+
+# Load the virtualenv containing the pytorch package
+source ~/venv/bin/activate
+
+
+for lr in 0.001 0.003 0.005 0.0001 0.0003 0.0005 0.00001 0.00003 0.00005; do
+python train_lstm.py --n_elems=29 \
+                     --n_train_elems=29 \
+                     --n_train_samples=3840 \
+                     --n_eval_samples=1000 \
+                     --n_epochs=100 \
+                     --n_layers=1 \
+                     --train_unique='' \
+                     --noise='.' \
+                     --log_folder='logs' \
+                     --seed=30  \
+                     --lr=$lr
+done;
---
+++ Evaluate Policy Network/train_lstm.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from utils import set_seed
+from utils import Dataset
+from utils import batch_accuracy, dataloader_accuracy
+from models import LSTM
+
+set_seed(30)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+n_piles = 5
+batch_size = 128
+eval_interval = 50
+num_layers = 1
+n_epochs = 100
+
+train_dataset = Dataset(n_samples=10000, n_piles=n_piles)
+test_dataset = Dataset(n_samples=2000, n_piles=n_piles)
+
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+dataloader_dict = {
+    'test': DataLoader(test_dataset, batch_size=batch_size),
+}
+
+lstm_model = LSTM(input_size=1,
+                  hidden_size=128,
+                  num_layers=num_layers)
+lstm_model = lstm_model.to(device)
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(lstm_model.parameters(), lr=0.001)
+writer = SummaryWriter(f'logs/{n_piles}_{num_layers}')
+
+
+num_steps = 0
+for num_epoch in range(n_epochs):
+    print(f'Epochs: {num_epoch}')
+    for X_batch, y_batch in train_dataloader:
+
+        X_batch = X_batch.to(device)
+        y_batch = y_batch.to(device)
+        y_pred_batch = lstm_model(X_batch)
+        train_batch_acc = batch_accuracy(y_pred_batch, y_batch)
+        writer.add_scalar('train_batch_accuracy', train_batch_acc, num_steps)
+
+        loss = criterion(y_pred_batch, y_batch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (num_steps % eval_interval) == 0:
+            for loader_name, loader in dataloader_dict.items():
+                val_acc = dataloader_accuracy(loader, lstm_model)
+                writer.add_scalar(loader_name, val_acc, num_steps)
+
+        num_steps += 1
+
+
